1d93e3587ddd52018ca320902c69aba65844deef
itmighthavebeen <122944777+itmighthavebeen@users.noreply.github.com>
2025-01-07 00:15:54 -0500

    First commit-completed assignment

--- js/index.js
+++ js/index.js
@@ -1,4 +1,48 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+//experiment section
+/*console.log(1 && 3);
+console.log(9 % 2);    //should be 1
+console.log(9 % (-2));   // should be neg
+var result_what = 9 % -2; // result = -1
+console.log (result_what);
+var a = 0;
+  a ++ ; /* a = a + 1, The Increment operator, 1 */
+/*  console.log("a is 1:", a);
+ var b = 10;
+  b --; /* b = b - 1, The Decrement operator, 9 */
+/*  console.log("b is 9:",b);
+ var c = 50; 
+  c += 10; /* c = c + 10, 60 */
+/*console.log("c is 60:", c);
+ var d = "Hello";
+  d += "!" /* d = d + "!" , Add (concatenate) strings, Hello! */
+/*  console.log("d:", d);
+ var x = a + b; /* The addition operator, 10 */
+// var y = a * b /* The Multiplication operator, 9 */
+///var z = a / b /* The Division operator, 0.1111111111111111 */
+// var w = a % b; /* The Modular operator (%) returns the division remainder, 1 */
+ 
+	// JavaScript code to perform modulo (%) 
+	// operation on negative numbers 
+		
+	// Use mod() function 
+/*	Number.prototype.mod = function(a) { 
+		
+		// Calculate 
+		return this % a; 
+	} 
+		
+	// Driver code 
+	var x = -21; 
+	var b = 4; 
+	
+	// Call mod() 
+	var result = x.mod(b); 
+		
+	// Print result 
+	console.log("The outcome is -1 right?: " + result); 
 
+ console.log ("should be 10,9,0.11111, 1", x,y,z,w)
+ */
